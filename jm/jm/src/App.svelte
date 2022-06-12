c0fc29009057ecce273dbb2118b4ff9a2fcb834e
JettiJAM <jettijam@gmail.com>
2022-06-12 21:18:31 +0900

    svelte template

---
+++ jm/jm/src/App.svelte
@@ -0,0 +1,39 @@
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+	export let name;
+</script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+<main>
+	<table class="table table-hover">
+		<thead>
+			<tr>
+				<th scope="col" class="text-center">#</th>
+				<th scope="col" class="text-center">제목</th>
+				<th scope="col" class="text-center">작성일</th>
+				<th scope="col" class="text-center">조회수</th>
+			</tr>
+		</thead>
+	</table>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
